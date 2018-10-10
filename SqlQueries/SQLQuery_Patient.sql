71f645e45271b7e53ac0f3be4c6b4cda7b531b3f
NarasimhaMurty <43975904+NarasimhaMurty@users.noreply.github.com>
2018-10-10 16:07:06 +0530

    UpdateSQL
    UpdateSQL

---
+++ SqlQueries/SQLQuery_Patient.sql
@@ -0,0 +1,33 @@
+/* To prevent any potential data loss issues, you should review this script in detail before running it outside the context of the database designer.*/
+BEGIN TRANSACTION
+SET QUOTED_IDENTIFIER ON
+SET ARITHABORT ON
+SET NUMERIC_ROUNDABORT OFF
+SET CONCAT_NULL_YIELDS_NULL ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+COMMIT
+BEGIN TRANSACTION
+GO
+CREATE TABLE dbo.Patient
+	(
+	UserID int NOT NULL,
+	FirstName varchar(50) NOT NULL,
+	LastName varchar(50) NOT NULL,
+	EmailID varchar(254) NOT NULL,
+	DateOfBirth datetime NULL,
+	Password nvarchar(MAX) NOT NULL	
+	)  ON [PRIMARY]
+	 TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE dbo.Patient SET (LOCK_ESCALATION = TABLE)
+GO
+COMMIT
+
+ALTER TABLE dbo.Patient
+ADD CONSTRAINT UserIDConstraint PRIMARY KEY(UserID)
+
+Drop table dbo.Patient
+
+select * from dbo.Patient
